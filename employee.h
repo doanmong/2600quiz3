560c87f87f8d0e55805f5686565e26813411fe93
Doan Mong <ddmong@cpp.edu>
2022-10-23 02:55:38 -0700

    fixed

--- employee.h
+++ employee.h
@@ -1,12 +1,9 @@
-#include <stdio.h> 
-#include <stddef.h> 
-
-typedef struct { 
-    long number; 
-    char *name; 
-    char *phone; 
-    double salary; 
-
-} Employee, *PtrToEmployee; 
-
-typedef const Employee *PtrToConstEmployee; 
+#include <stdio.h>
+#include <stddef.h>
+typedef struct {
+    long number;
+    char *name;
+    char *phone;
+    double salary;
+} Employee, *PtrToEmployee;
+typedef const Employee *PtrToConstEmployee;
